762f5023bc6fd62252c4764bc83fd3defaadd333
Christopher Kodama <ckhordiasma@gmail.com>
2021-07-29 00:26:13 -0400

    added more error messages and fixed issue
    sometimes the mime format starts with different things at the beginning, I guess, so I added some code to handle different cases.
    
    also added more helpful error messages to save me time when troubleshooting issues.

--- mrs-smime.sh
+++ mrs-smime.sh
@@ -25,33 +25,45 @@ if [ $? -ne 0 ]; then { echo 'Output folder was not selected' ; exit 1; } fi
 echo $outFolder
 
 
-echo "Decrypting and decoding file ..."
+echo "Decrypting file ..."
 decrypted="$outFolder/decrypted.smime"
 
 # - Decrypts file using native mac stuff
+security cms -D -i "$p7mFile" \
+  > $decrypted
+
+if [ $? -ne 0 ] ; then {
+  echo 'Failed to decrypt file'; 
+  exit 1;
+} fi
+
+decoded="$outFolder/decoded.smime"
+# check to see if file is already decoded
+
+final=$decrypted
 # - Removes MIME headers (sed: delete (/d) all lines from line 1 to pattern: line with nothing in it)
 # - Removes new lines 
 # - Decodes out of base 64 
 # - Takes out everything before "MIME-Version" (the signature bytes) on the first line
+# - Same but for Conent-Type on first line
 # - Outputs to 'decrypted.smime' in the selected output folder
-security cms -D -i "$p7mFile" \
-  > $decrypted
-
-# check to see if file is already decoded
-if !  grep -q 'Content-Type: text/plain' $decrypted; then
+if !  grep -q 'Content-Type: text/plain' $decrypted; then {
+  echo "Decoding multi-part smime file..."
   cat $decrypted \
   | sed '1,/^\s*\r$/d' \
   | sed 's/\r$//' \
   | base64 --decode \
   | perl -pe '$. == 1? s/^.*(MIME-Version: [\d\.]+)/$1/ : "" ' \
-  > $decrypted
-fi
+  | perl -pe '$. == 1? s/^.*(Content-Type: .+)/$1/ : "" ' \
+  > $decoded
+  final=$decoded
+} fi
 
-if [ $? -ne 0 ] || [ ! -s "$decrypted" ] ; then { echo 'Failed to decrypt or decode file' ; exit 1; } fi
+if [ $? -ne 0 ]  ; then { echo 'Failed to decode file (first pass)' ; exit 1; } fi
 
 
 echo "Decoding file..."
-python3 "$(dirname $0)/mimesplit.py" -d "$outFolder" "$decrypted"
-if [ $? -ne 0 ] ; then { echo 'Failed to decode file' ; exit 1; } fi
+python3 "$(dirname $0)/mimesplit.py" -d "$outFolder" "$final"
+if [ $? -ne 0 ] ; then { echo 'Failed to decode file (second pass)' ; exit 1; } fi
 
 echo "Script complete. Files located at '$outFolder'"
